b2c9addbbd6854314af8de2e529e34964489675f
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-02-07 14:10:22 +0100

    Added Gif, GifList and SearchBar component

---
+++ src/components/Gif.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    return (
+      <div>
+        This is a gif
+      </div>
+    );
+  ;}
+};
+
+export default Gif;
---
+++ src/components/GifList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class GifList extends Component {
+  render() {
+    return (
+      <div>
+        This is a gif list
+      </div>
+    );
+    ;
+  }
+};
+
+export default GifList;
